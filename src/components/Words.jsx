3b112e7ccbf8ca8bbbc087c6f9ccd41d6da96f33
alikpashaevi <alikpashaev06@gmail.com>
2024-08-05 00:55:00 +0400

    third commit

--- src/components/Words.jsx
+++ src/components/Words.jsx
@@ -1,28 +1,42 @@
 import React from 'react';
 
-const Words = ({ words, currentIndex, letterIndex, inputError }) => {
+const Words = ({ words, currentIndex, letterIndex, inputError, wordStatus }) => {
   return (
     <div>
       <h1>Words List</h1>
       <div className="word-list">
-        {words.map((word, index) => (
-          <span
-            key={index}
-            style={{
-              backgroundColor: index === currentIndex ? (inputError ? 'red' : 'green') : 'white',
-              fontWeight: index === currentIndex ? 'bold' : 'normal',
-            }}
-          >
-            {word.split('').map((letter, i) => (
-              <span
-                key={i}
-              >
-                {letter}
-              </span>
-            ))}
-            {' '}
-          </span>
-        ))}
+      {words.map((word, index) => {
+          let backgroundColor = 'transparent';
+          let color = 'white';
+
+          if (index === currentIndex) {
+            backgroundColor = inputError ? 'red' : 'grey';
+          } else if (index < currentIndex) {
+            if (wordStatus[index] === 'correct') {
+              color = 'green';
+            } else if (wordStatus[index] === 'incorrect') {
+              // Reset background color for incorrect words after moving to the next word
+              color = 'red'
+              backgroundColor = 'transparent';
+            }
+          }
+
+          return (
+            <span
+              key={index}
+              style={{
+                backgroundColor,
+                color,
+                fontWeight: index === currentIndex ? 'bold' : 'normal',
+              }}
+            >
+              {word.split('').map((letter, i) => (
+                <span key={i}>{letter}</span>
+              ))}
+              {' '}
+            </span>
+          );
+        })}
       </div>
     </div>
   );
--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -1,12 +1,16 @@
-import React, {useEffect, useState} from 'react'
+import React, { useEffect, useState } from 'react';
 import Words from '../components/Words';
 import InputArea from '../components/InputField';
+import  './MainPage.css';
 
 const MainPage = () => {
   const [words, setWords] = useState([]);
   const [currentIndex, setCurrentIndex] = useState(0);
   const [letterIndex, setLetterIndex] = useState(0);
   const [inputError, setInputError] = useState(false);
+  const [correctWords, setCorrectWords] = useState(0);
+  const [incorrectWords, setIncorrectWords] = useState(0);
+  const [wordStatus, setWordStatus] = useState([]); // Array to track correctness of words
 
   useEffect(() => {
     fetch('data/words.json')
@@ -17,12 +21,18 @@ const MainPage = () => {
 
   const checkWord = (inputWord) => {
     if (words[currentIndex] === inputWord.trim()) {
-      setCurrentIndex((prevIndex) => prevIndex + 1);
-      setLetterIndex(0); // Reset letter index for next word
+      setCorrectWords((prevCount) => prevCount + 1);
       setInputError(false);
+      setWordStatus((prevStatus) => [...prevStatus, 'correct']);
+    } else {
+      setIncorrectWords((prevCount) => prevCount + 1);
+      setInputError(true);
+      setWordStatus((prevStatus) => [...prevStatus, 'incorrect']);
     }
-  };
+    setCurrentIndex((prevIndex) => prevIndex + 1);
+    setLetterIndex(0); // Reset letter index for next word
 
+  };
 
   const checkLetter = (inputLetter) => {
     if (inputLetter === words[currentIndex][letterIndex]) {
@@ -30,6 +40,12 @@ const MainPage = () => {
       setInputError(false); // Correct letter, no error
     } else {
       setInputError(true); // Incorrect letter, set error state
+      if (letterIndex === words[currentIndex].length - 1) {
+        setLetterIndex(0); // Reset letter index for next word
+        setInputError(false);
+      } else {
+        setLetterIndex((prevIndex) => prevIndex + 1);
+      }
     }
   };
 
@@ -40,11 +56,13 @@ const MainPage = () => {
         currentIndex={currentIndex}
         letterIndex={letterIndex}
         inputError={inputError}
+        wordStatus={wordStatus} // Pass the wordStatus to Words component
       />
       <InputArea checkWord={checkWord} checkLetter={checkLetter} />
+      <div>Correct Words: {correctWords}</div>
+      <div>Incorrect Words: {incorrectWords}</div>
     </div>
   );
-
 }
 
-export default MainPage
+export default MainPage;
