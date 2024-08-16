280c7ca991097691b07a9d66f9a924207369087b
alikpashaevi <alikpashaev06@gmail.com>
2024-08-16 23:47:42 +0400

    added chart

--- client/src/components/profile/Progress.jsx
+++ client/src/components/profile/Progress.jsx
@@ -1,9 +1,27 @@
 import React from 'react'
+import { Line } from "react-chartjs-2";
+import './Progress.css'
 
-const Progress = () => {
+const Progress = ({chartData}) => {
   return (
     <div className='progress-container'>
-      Progress
+      <div className="chart-container">
+      <h2 style={{ textAlign: "center" }}>Your Progress</h2>
+      <Line
+        data={chartData}
+        options={{
+          plugins: {
+            title: {
+              display: true,
+              text: "Your typing speed progression so far"
+            },
+            legend: {
+              display: false
+            }
+          }
+        }}
+      />
+    </div>
     </div>
   )
 }
--- client/src/pages/ProfilePage.jsx
+++ client/src/pages/ProfilePage.jsx
@@ -4,8 +4,26 @@ import Usercard from '../components/profile/UserCard'
 import './ProfilePage.css'
 import Elebot from '../components/profile/Elebot'
 import Progress from '../components/profile/Progress'
+import { useState } from 'react'
+import Chart from "chart.js/auto";
+import { CategoryScale } from "chart.js";
+
+Chart.register(CategoryScale);
 
 const ProfilePage = () => {
+  const [chartData, setChartData] = useState( {
+    labels: ['2016', '2017', '2018', '2019', '2020'], // Labels for the x-axis
+    datasets: [
+      {
+        label: 'WPM',
+        data: [60, 90, 80, 93, 87, 98, 85], // Data for the y-axis
+        backgroundColor: 'rgba(75,192,192,0.6)',
+        borderColor: 'rgba(75,192,192,1)',
+        borderWidth: 2,
+      },
+    ],
+  });
+
   return (
     <main className='profile-page-main'>
       <Header />
@@ -13,7 +31,7 @@ const ProfilePage = () => {
         <Usercard />
         <div className="others-div">
           <Elebot />
-          <Progress />
+          <Progress chartData={chartData} />
         </div>
       </div>
     </main>
